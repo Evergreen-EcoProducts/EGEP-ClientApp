ba17d97db0e7898fa7b10ba4ed3bdf554e10a647
Dhinesh Venkitasamy <Dhinesh.Venkitasamy2@gmail.com>
2023-10-23 11:50:20 +0530

    product detail changes

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpApiService } from '../http-api.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,42 +8,32 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  @Input() productDetail: any;
-  selectedId: number = 1;
+  productDetail: any;
+  selectedId: number = 0;
+  selectedImageId: number = 1;
   selectedQuantity: number = 1;
-  constructor() { }
+  selectedPrice: any;
+  constructor(private route: ActivatedRoute, private httpApiService: HttpApiService) { }
 
-  ngOnInit(): void {
-    this.productDetail = {
-      "id": 1,
-      "productImages": [
-        {
-          "id": 1,
-          "src": "./assets/img/product-1.png",
-          "thumbNailSrc": "http://placekitten.com/200/126",
-        },
-        {
-          "id": 2,
-          "src": "./assets/img/product-4.png",
-          "thumbNailSrc": "http://placekitten.com/200/126",
-        },
-        {
-          "id": 3,
-          "src": "./assets/img/product-2.png",
-          "thumbNailSrc": "http://placekitten.com/200/126",
-        },
-        {
-          "id": 4,
-          "src": "./assets/img/product-3.png",
-          "thumbNailSrc": "http://placekitten.com/200/126",
-        },
-        {
-          "id": 5,
-          "src": "./assets/img/product-4.png",
-          "thumbNailSrc": "http://placekitten.com/200/126",
-        }
-      ]
+  async ngOnInit() {
+    this.selectedId = this.route.snapshot.params['id'];
+    if (!!this.selectedId) {
+      await this.getProductDetail();
     }
-  }
 
+  }
+  async getProductDetail() {
+    await this.httpApiService.getProducts().toPromise().then((res: any | undefined) => {
+      if (res && res.products) {
+        this.productDetail = res.products.find((product: any) => {
+          return product.id == Number(this.selectedId);
+        });
+        console.log(this.productDetail);
+      }
+    });
+  }
+  onSizeChange(event: any) {
+    this.selectedQuantity = event.size;
+    this.selectedPrice = event.amount;
+  }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpApiService } from '../http-api.service';
 declare var jQuery: any;
 @Component({
   selector: 'app-product',
@@ -7,12 +9,16 @@ declare var jQuery: any;
 })
 export class ProductComponent implements OnInit {
   Images = ['assets/img/product-1.png', 'assets/img/product-2.png', 'assets/img/product-3.png'];
-
+  products: any;
   SlideOptions = { items: 1, dots: true, nav: true };
-  CarouselOptions = { items: 3, dots: true, nav: true };  
-  constructor() { }
+  CarouselOptions = { items: 3, dots: true, nav: true, navText: ['<i class="bi bi-arrow-left-circle"></i>', '<i class="bi bi-arrow-right-circle"></i>'] };
+  
+  constructor(private httpApiService: HttpApiService, private router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.getProducts();
   }
   ngAfterViewInit() {
     (function ($) {
@@ -45,5 +51,20 @@ export class ProductComponent implements OnInit {
         });
       });
     })(jQuery);
+  }
+  async getProducts() {
+    await this.httpApiService.getProducts().toPromise().then((res: any[] | undefined) => {
+      if (res) {
+        this.products = res;
+      }
+    });
+  }
+  onProductDetail(id:any) {
+    this.router.navigate(['product-detail/' + id]);
+    window.scroll({
+      top: 700,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 }
